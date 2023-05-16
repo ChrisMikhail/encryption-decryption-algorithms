89ee1cf21fbd419962c3152a7c84362126e705c3
Chris Mikhail <samuelmikhc@gmail.com>
2023-05-16 14:24:56 -0400

    caeser cipher program

---
+++ CaesarCipher.py
@@ -0,0 +1,23 @@
+# Caeser Cipher encryption/decryption program
+
+def encodeOrDecode(method, message):
+    letters = {i + 97: chr(ord('a') + i) for i in range(26)}
+    newMsg = ""
+    for char in message:    
+        if char != " ":
+            if method == "e" and ord(char) < 119:
+                newMsg += letters[ord(char) + 4]               
+            elif ord(char) > 119:
+                newMsg += letters[ord(char) - 22]
+            elif method == "d" and ord(char) > 100:
+                newMsg += letters[ord(char) - 4]
+            elif ord(char) < 100:
+                newMsg += letters[ord(char) + 22]
+        else:
+            newMsg += " "
+    return newMsg
+
+if __name__ == "__main__":
+    method = input("Hello! This is the Caesar Cipher program! Would you like to encrypt or decrypt a message? (enter encrypt or decrypt) ").lower()
+    message = input(f"Great choice! Please enter the phrase that you want to {method} (do not include numbers or symbols except for spaces): ").lower()
+    print(encodeOrDecode(method, message))
