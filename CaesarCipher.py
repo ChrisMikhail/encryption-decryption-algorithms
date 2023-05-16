87d0c5b0eaab91b23fb5c72efb76bf01581651d9
Chris Mikhail <samuelmikhc@gmail.com>
2023-05-16 14:25:54 -0400

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -18,6 +18,6 @@ def encodeOrDecode(method, message):
     return newMsg
 
 if __name__ == "__main__":
-    method = input("Hello! This is the Caesar Cipher program! Would you like to encrypt or decrypt a message? (enter e or d) ").lower()
+    method = input("Hello! This is the Caesar Cipher program! Would you like to encrypt or decrypt a message? (enter e or d): ").lower()
     message = input(f"Great choice! Please enter the phrase that you want to {method} (do not include numbers or symbols except for spaces): ").lower()
     print(encodeOrDecode(method, message))
